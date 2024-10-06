b5ceeca5bfbf6f6855d38de1ee9c2f50e4027478
Rafael M Cunha <me@rafaelmc.net>
2024-10-06 18:48:14 +0200

    refactor post listing to separate method

--- main/blog.py
+++ main/blog.py
@@ -1,6 +1,31 @@
+import os
+from pathlib import Path
+
+import markdown
+
 from flask import Blueprint, render_template
 
 bp = Blueprint("blog", __name__, url_prefix="/blog")
+
+
+def get_all_posts_meta():
+    posts_path = Path(__file__).resolve().parent.parent / "posts"
+    files = sorted(
+        filter(lambda x: x.endswith(".md"), os.listdir(posts_path)), reverse=True
+    )
+    posts = []
+    for file in files:
+        with open(posts_path / file) as f:
+            md = markdown.Markdown(extensions=["meta"])
+            md.convert(f.read())
+            posts.append(
+                {
+                    "title": md.Meta["title"][0],
+                    "date": md.Meta["date"][0],
+                    "url": "/blog/post/" + file[:-3],
+                }
+            )
+    return posts
 
 
 @bp.route("/")
--- main/main.py
+++ main/main.py
@@ -1,35 +1,16 @@
-import os
-from pathlib import Path
-
-import markdown
-
 from flask import Blueprint, render_template
+
+from main.blog import get_all_posts_meta
 
 
 bp = Blueprint("main", __name__)
 
-ARTICLES_PATH = Path(__file__).resolve().parent.parent / "posts"
-
 
 @bp.route("/")
 def index():
-    files = sorted(
-        filter(lambda x: x.endswith(".md"), os.listdir(ARTICLES_PATH)), reverse=True
-    )
-    meta = []
-    for file in files:
-        with open(ARTICLES_PATH / file) as f:
-            md = markdown.Markdown(extensions=["meta"])
-            md.convert(f.read())
-            meta.append(
-                {
-                    "title": md.Meta["title"][0],
-                    "date": md.Meta["date"][0],
-                    "url": "/blog/post/" + file[:-3],
-                }
-            )
+    posts = get_all_posts_meta()
 
-    return render_template("main/index.html", posts=meta)
+    return render_template("main/index.html", posts=posts)
 
 
 @bp.route("/up")
