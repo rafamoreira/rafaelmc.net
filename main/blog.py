67be83332b87ce6ee471fda1d76e4089c9b3f409
Rafael M Cunha <me@rafaelmc.net>
2024-10-06 19:15:54 +0200

    render posts

--- main/blog.py
+++ main/blog.py
@@ -7,15 +7,16 @@ from flask import Blueprint, render_template
 
 bp = Blueprint("blog", __name__, url_prefix="/blog")
 
+POSTS_PATH = Path(__file__).resolve().parent.parent / "posts"
+
 
 def get_all_posts_meta():
-    posts_path = Path(__file__).resolve().parent.parent / "posts"
     files = sorted(
-        filter(lambda x: x.endswith(".md"), os.listdir(posts_path)), reverse=True
+        filter(lambda x: x.endswith(".md"), os.listdir(POSTS_PATH)), reverse=True
     )
     posts = []
     for file in files:
-        with open(posts_path / file) as f:
+        with open(POSTS_PATH / file) as f:
             md = markdown.Markdown(extensions=["meta"])
             md.convert(f.read())
             posts.append(
@@ -33,3 +34,18 @@ def index():
     posts = get_all_posts_meta()
 
     return render_template("blog/index.html", posts=posts)
+
+
+@bp.route("/post/<post>")
+def post(post):
+    with open(POSTS_PATH / (post + ".md")) as f:
+        md = markdown.Markdown(extensions=["meta", "fenced_code", "codehilite"])
+        html = md.convert(f.read())
+
+        post = {
+            "title": md.Meta["title"][0],
+            "date": md.Meta["date"][0],
+            "content": html,
+        }
+
+    return render_template("blog/post.html", post=post)
