723c04fada6e4d1709bf1156fd9f89f218cdcdd4
Rafael M Cunha <me@rafaelmc.net>
2024-10-06 02:01:13 +0200

    some sanity

--- main/main.py
+++ main/main.py
@@ -1,12 +1,20 @@
+import os
+from pathlib import Path
+
 from flask import Blueprint, render_template
 
 
 bp = Blueprint("main", __name__)
 
+ARTICLES_PATH = Path(__file__).resolve().parent.parent / "articles"
+
 
 @bp.route("/")
 def index():
-    return render_template("main/index.html")
+    markdown_files = [f for f in os.listdir(ARTICLES_PATH) if f.endswith(".md")]
+    files = sorted(markdown_files, reverse=True)[:5]
+    Markdown
+    return render_template("main/index.html", articles=articles)
 
 
 @bp.route("/up")
