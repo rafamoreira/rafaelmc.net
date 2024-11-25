9184fb3910f18e08d1e020a7c079d983d185b5c8
Rafael M Cunha <me@rafaelmc.net>
2024-11-25 01:52:28 +0100

    add new stats handling

--- main/__init__.py
+++ main/__init__.py
@@ -1,11 +1,13 @@
+import datetime
 import os
 
-from flask import Flask
+from flask import Flask, request
+import simplejson
 
 SITE_URL = "https://rafaelmc.net"
 
 
-def create_app(test_config=None):
+def create_app(test_config=None, test_mode=False):
     # create and configure the app
     app = Flask(__name__, instance_relative_config=True)
     app.config.from_mapping(
@@ -13,12 +15,34 @@ def create_app(test_config=None):
         # DATABASE=os.path.join(app.instance_path, 'flaskr.sqlite'),
     )
 
+    if test_mode:
+        stats_path = os.path.join(app.instance_path, "stats.json")
+    else:
+        stats_path = os.path.join("var", "lib", "rafaelmc", "stats.json")
+
     if test_config is None:
         # load the instance config, if it exists, when not testing
         app.config.from_pyfile("config.py", silent=True)
     else:
         # load the test config if passed in
         app.config.from_mapping(test_config)
+
+    @app.after_request
+    def after_request_handler(response):
+        if response.status_code == 200:
+            try:
+                with open(stats_path) as f:
+                    stats = simplejson.loads(f.read())
+            except FileNotFoundError:
+                stats = {"hits": {}}
+
+            stats["hits"][request.url] = stats["hits"].get(request.url, 0) + 1
+            stats["last_updated"] = datetime.datetime.now().isoformat()
+
+            with open(stats_path, "w") as f:
+                f.write(simplejson.dumps(stats))
+
+        return response
 
     # ensure the instance folder exists
     try:
